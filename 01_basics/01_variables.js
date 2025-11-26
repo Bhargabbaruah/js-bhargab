12c412ca0821ed43ab631b4bcc7e11324bd61fce
Bhargab Baruah <112149301+Bhargabbaruah@users.noreply.github.com>
2025-11-26 17:13:28 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "bhargab@google.com"
+let accountPassword = "12345"
+let accountCity = "guwahati"
+
+console.log(accountId);
+console.table([ accountId,accountEmail ,accountPassword ,accountCity]);
+
+
