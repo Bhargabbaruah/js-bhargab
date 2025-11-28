aed27fbca525b99639f49d1b0d039ee9c35e819e
Bhargab Baruah <112149301+Bhargabbaruah@users.noreply.github.com>
2025-11-28 18:32:32 +0000

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+console.log("Bhargab")
+let name = "bhargab"
+let age = 21
+let isLoggedIn = false
+
+// number ==> 2 to power 53 
+// bigint ==> ""
+// boolean ==> true/false 
+// null ==> standalone value
+// undefined ==>
+// symbol ==> unique
+
+
+// object 
+
+console.log(typeof "bhargab");
+
+
+
