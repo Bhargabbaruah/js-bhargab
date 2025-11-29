0ae7b4604a960cadae103aee3ce3e03d9076d78d
Bhargab Baruah <112149301+Bhargabbaruah@users.noreply.github.com>
2025-11-29 15:34:25 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc" 
+let score2 = 33
+console.log(typeof score);
+console.log(typeof (score2));
+
+let valuInNumber =Number(score);
+
+console.log (typeof valuInNumber);
+console.log(valuInNumber);
+
+
+// "33" =>33 
+// "33abc" => Nan
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsloggedIn);
+
+let isLoggedIn2 = "bhargab"
+let booleanIsloggedIn2 = Boolean(isLoggedIn2);
+
+console.log(booleanIsloggedIn2);
+
+// 1 => true;
+// 0 => false;
+// bhargab => true
